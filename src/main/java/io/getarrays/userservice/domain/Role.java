95d0c977863586c95bff34742f70c30177ea4a54
yabdioglu <yabdioglu37@gmail.com>
2022-10-10 00:47:35 +0300

    01-spring-boot-backend

--- src/main/java/io/getarrays/userservice/domain/Role.java
+++ src/main/java/io/getarrays/userservice/domain/Role.java
@@ -4,21 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
-public class User {
+public class Role {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
-
-    private String username;
-
-    private String password;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    private Collection<Role> roles = new ArrayList<>();
 }
